d997a843102970f6672d239134b97f6e1ad88f5d
Ralf Boltshauser <ralf@boltshauser.com>
2025-02-22 21:17:11 +0100

    full width custom blocks

--- src/blocks/MarkdownBlock/Component.tsx
+++ src/blocks/MarkdownBlock/Component.tsx
@@ -11,7 +11,7 @@ export const MarkdownBlock: React.FC<MarkdownBlockProps> = ({ markdownContent })
     <div className="container my-16">
       <div className="grid grid-cols-4 lg:grid-cols-12 gap-y-8 gap-x-16">
         <div className="col-span-4 lg:col-span-12">
-          <div className="prose dark:prose-invert">
+          <div className="prose dark:prose-invert w-full">
             <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdownContent}</ReactMarkdown>
           </div>
         </div>
